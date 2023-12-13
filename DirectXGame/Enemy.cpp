0df036e247d3980e561f2832415d023fc22e31a3
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2023-12-13 16:29:02 +0900

    敵途中

---
+++ DirectXGame/Enemy.cpp
@@ -0,0 +1,21 @@
+#include "Enemy.h"
+
+void Enemy::Initialize(Model* model) {
+	// 引数からデータを受け取る
+	model_ = model;
+	// ワールド変換の初期化
+	worldTransform_.Initialize();
+	worldTransform_.scale_ = {1.0f, 1.0f, 1.0f};
+	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
+	worldTransform_.translation_ = {0.0f, 0.0f, 50.0f};
+}
+
+void Enemy::Update() {
+	// 行列の更新
+	worldTransform_.UpdateMatrix();
+}
+
+void Enemy::Draw(ViewProjection& viewProjection) {
+
+	model_->Draw(worldTransform_, viewProjection);
+}
