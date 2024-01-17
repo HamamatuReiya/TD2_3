2acccc7c3f45564bbd44a282efebca4c445ef881
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2024-01-17 15:01:16 +0900

    途中

---
+++ DirectXGame/Borderline.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+class Borderline {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private:
+	
+
+};
