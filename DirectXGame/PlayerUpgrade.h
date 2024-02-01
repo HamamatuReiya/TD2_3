0440884f33930731bdc90780db6d897b86a2a2e2
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2024-02-01 10:14:02 +0900

    プレイヤーアップグレードに関数追加

--- DirectXGame/PlayerUpgrade.h
+++ DirectXGame/PlayerUpgrade.h
@@ -4,12 +4,17 @@ class PlayerUpgrade {
 public:
 	//強化内容
 	//攻撃力
-	void AttackUpgrade();
+	void AttackUpgrade(int attackPower);
 	//体力上昇
-	void HitPointUpgrade();
+	void HitPointUpgrade(int playerHP);
 	//回復力の上昇
-	void RecoveryUpgrade();
+	void RecoveryUpgrade(int recovery);
 
 private:
-
+	//加算する攻撃力
+	int attack_ = 1;
+	//体力
+	int hitPoint_ = 2;
+	//回復力
+	int recovery_ = 1;
 };
