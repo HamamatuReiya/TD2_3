85b4247992389a35aea85baeafd18d33f16aff5c
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2024-02-08 06:02:47 +0900

    ウィンドウ名変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -27,7 +27,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow(L"2346_病原体スレイヤー");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
